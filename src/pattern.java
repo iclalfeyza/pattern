2ad006da64378983da4c3710447c7e2b1a7a8c4d
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-12-11 20:04:04 +0300

    last

--- src/pattern.java
+++ src/pattern.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class pattern {
+import java.util.Scanner;
+
+public class pattern {
+    static void function(int n, boolean trueFalse, int temp) {
+
+        if ((trueFalse == true) && (n <= temp) && (n > 0)) {
+            System.out.print(n + " ");
+            n -= 5;
+            if (n <= 0) {
+                function(n, false, temp);
+            } else {
+                function(n, true, temp);
+            }
+        } else if ((trueFalse == false) && (n <= temp)) {
+            System.out.print(n + " ");
+            n += 5;
+            function(n, false, temp);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int num = input.nextInt();
+        int temp = num;
+        function(num, true, temp);
+    }
 }
